a393f2cbac479c20cef1da33de2e2b1ce8143e3a
Maxime LE BONNIEC <maxime.lbc.pro@gmail.com>
2021-12-10 21:00:01 +0100

    ✨ Add : the last time theme choice is now applied. For that local storage is used.

--- src/client/features/Header/components/ThemeSwitch.tsx
+++ src/client/features/Header/components/ThemeSwitch.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from 'react';
+import { useLocalStorage } from 'react-use';
 import { useAppDispatch, useAppSelector } from '../../../../global/redux/hooks';
 import { switchTheme } from '../../../../global/redux/slices/themeSlice';
+import { Themes } from '../../../../global/enums/themes.enum';
 
 const urls = {
   light: '/assets/images/sun.svg',
@@ -10,10 +13,25 @@ const urls = {
 const ThemeSwitch = () => {
   const theme = useAppSelector(state => state.themeReducer.value);
   const dispatch = useAppDispatch();
+  const [value, setValue, remove] = useLocalStorage(
+    'alcanderia.theme',
+    Themes.LIGHT,
+  );
+
+  // Put theme choice in the local storage for next time
+  useEffect(() => {
+    if (theme === 'light') setValue(Themes.LIGHT);
+    if (theme === 'dark') setValue(Themes.DARK);
+  }, [theme]);
+
+  // Apply last time theme choice
+  useEffect(() => {
+    if (value === Themes.DARK) dispatch(switchTheme());
+  }, []);
 
   return (
     <div
-      className="px-5 mr-5 relative inline-block w-16 h-7 bg-gray-250 dark:bg-gray-600 rounded-full shadow-inner cursor-pointer"
+      className="px-5 mr-5 relative inline-block w-16 h-7 bg-gray-600 dark:bg-gray-600 rounded-full cursor-pointer"
       onClick={() => dispatch(switchTheme())}
     >
       <img
